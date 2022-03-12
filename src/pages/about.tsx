59ca04109591c23cd1ce5007aa6b6bceb4bbf92b
Ava Katushka <ava8katushka@gmail.com>
2022-03-12 19:01:45 +0300

    Add react layout

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle={"About me"}>
+        <p>This is a page about sexy little baby, me!</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
