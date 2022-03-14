042696fe3d73e4c15f4592c480dbd45bd7cbfe8c
Ava Katushkae <ava8katushka@gmail.com>
2022-03-14 09:09:25 +0300

    Add images

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,15 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle={"About me"}>
         <p>This is a page about sexy little baby, me!</p>
+        <StaticImage
+        alt="Ava in New York"
+        src="https://drive.google.com/uc?export=view&id=1eHm0tF1n-dONlaOMUtSi1MMnAo0l8Zab"
+      />
     </Layout>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Ava's blog">
       <p>Fuck you, bitches!!!</p>
+      <StaticImage
+        alt="Fuck the reality"
+        src="https://images.unsplash.com/photo-1582324597727-186650f42085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+      />
     </Layout>
   )
 }
