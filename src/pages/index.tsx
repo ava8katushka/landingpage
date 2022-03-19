7310b5687e5cd4f4e7a6d5acf683c2cc5edf7d47
Ava Katushkae <ava8katushka@gmail.com>
2022-03-19 21:08:27 +0300

    Add email collection form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import EmailCollector from "../components/emailCollector"
 
 const IndexPage = () => {
   return (
@@ -10,6 +11,10 @@ const IndexPage = () => {
         alt="Time and Space complexity of Algorithms"
         src="https://res.cloudinary.com/ava-coding-com/image/upload/v1647240152/Algorithms%20Book/1st_opening_ooh6yr.png"
       />
+      <p>
+        If you want to receive new chapters of the book, enter your email here:
+      </p>
+      <EmailCollector></EmailCollector>
     </Layout>
   )
 }
