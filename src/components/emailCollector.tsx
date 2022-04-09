e4bc22aa3f89d0a9e6d111e5d65c50a6e491a7aa
Ava Katushkae <ava8katushka@gmail.com>
2022-04-09 13:22:19 +0300

    Put the form in the card

--- src/components/emailCollector.tsx
+++ src/components/emailCollector.tsx
@@ -1,29 +1,35 @@
 import * as React from "react"
 import "./emailCollector.module.css";
-import Button from '@mui/material/Button';
+import { Card, Button, Typography, CardContent} from '@mui/material';
 
 export default function AirtableUI() {
   return (
-    <form action="/api/airtable" method="POST">
-      <div style={{ marginBottom: `8px` }}>
-        <label style={{ display: `block`, marginBottom: `4px` }} htmlFor="name">
-          Name:
-        </label>
-        <input name="name" id="name" />
-      </div>
-      <div style={{ marginBottom: `8px` }}>
-        <label
-          style={{ display: `block`, marginBottom: `4px` }}
-          htmlFor="email"
-        >
-          Email:
-        </label>
-        <input name="email" id="email" type="email" />
-      </div>
-      <div>
-        {/* <button>Get the book</button> */}
-        <Button type="submit" variant="contained">Get the book</Button>
-      </div>
-    </form>
+    <Card sx={{ maxWidth: 500, p: '10px' }}>
+      <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+          I want to get the book!
+        </Typography>
+      <form action="/api/airtable" method="POST">
+        <div style={{ marginBottom: `8px` }}>
+          <label style={{ display: `block`, marginBottom: `4px` }} htmlFor="name">
+            Name:
+          </label>
+          <input name="name" id="name" />
+        </div>
+        <div style={{ marginBottom: `8px` }}>
+          <label
+            style={{ display: `block`, marginBottom: `4px` }}
+            htmlFor="email"
+          >
+            Email:
+          </label>
+          <input name="email" id="email" type="email" />
+        </div>
+        <div>
+          <Button type="submit" variant="contained">Get the book</Button>
+        </div>
+      </form>
+    </CardContent>
+    </Card>
   )
 }
