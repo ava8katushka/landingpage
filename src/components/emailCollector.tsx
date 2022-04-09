62b5c81c7439d45a29083c99b363e4f8c468aed1
Ava Katushkae <ava8katushka@gmail.com>
2022-04-09 13:45:07 +0300

    Move input fields to use MUI

--- src/components/emailCollector.tsx
+++ src/components/emailCollector.tsx
@@ -1,33 +1,24 @@
 import * as React from "react"
 import "./emailCollector.module.css";
-import { Card, Button, Typography, CardContent} from '@mui/material';
+import { Box, Card, Button, Typography, CardContent, TextField, CardActions} from '@mui/material';
 
 export default function AirtableUI() {
   return (
-    <Card sx={{ maxWidth: 500, p: '10px' }}>
+    <Card sx={{ maxWidth: '300px', p: '10px' }}>
       <CardContent>
       <Typography gutterBottom variant="h5" component="div">
           I want to get the book!
         </Typography>
       <form action="/api/airtable" method="POST">
-        <div style={{ marginBottom: `8px` }}>
-          <label style={{ display: `block`, marginBottom: `4px` }} htmlFor="name">
-            Name:
-          </label>
-          <input name="name" id="name" />
-        </div>
-        <div style={{ marginBottom: `8px` }}>
-          <label
-            style={{ display: `block`, marginBottom: `4px` }}
-            htmlFor="email"
-          >
-            Email:
-          </label>
-          <input name="email" id="email" type="email" />
-        </div>
-        <div>
-          <Button type="submit" variant="contained">Get the book</Button>
-        </div>
+      <Box sx={{ my: '15px'}}>
+      <TextField name="name" label="Name" variant="outlined"/>
+      </Box>
+      <Box sx={{ my: '15px'}}>
+      <TextField name="email" label="Email" variant="outlined"/>
+      </Box>
+      <Box sx={{ mt: '15px'}}>
+      <Button type="submit" variant="contained">Get the book</Button>
+      </Box>
       </form>
     </CardContent>
     </Card>
